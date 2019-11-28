4de7ddd9295aaf95a9b6ed80d337116530208ecd
Werner Robitza <werner.robitza@gmail.com>
2019-11-28 16:14:44 +0100

    add profiling test description

---
+++ DEVELOPERS.md
@@ -0,0 +1,18 @@
+# Developer Guide
+
+## Profiling
+
+Requirements:
+
+```
+apt install graphviz
+pip3 install gprof2dot
+```
+
+Run:
+
+```bash
+test/profile.sh
+```
+
+Check output.pdf for a graphical representation of the profiling output.
---
+++ test/profile.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Create a profile trace and output PDF
+
+cd "$(dirname "$0")"
+
+rm -f output.cprof output.pdf && \
+PYTHONPATH=. \
+python3 \
+    -m cProfile \
+        -s cumtime \
+        -o output.cprof \
+    ../itu_p1203/__main__.py \
+        --cpu-count 1 \
+        --print-intermediate \
+        ../examples/request.json \
+    > /dev/null 2>&1 && \
+gprof2dot \
+    -f pstats \
+    -z "p1203_standalone:201:calculate_complete" \
+    output.cprof | \
+dot -Tpdf -o output.pdf
