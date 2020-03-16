bdb717f8c4f10a03ecb5a2244712e19c4bd8258f
Werner Robitza <werner.robitza@gmail.com>
2020-03-16 09:39:51 +0100

    remove python 3.8 compatibility, see #16

--- setup.py
+++ setup.py
@@ -52,8 +52,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7'
     ],
     entry_points={
         'console_scripts': [
