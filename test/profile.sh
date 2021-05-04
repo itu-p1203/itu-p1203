42b7b502eb859a2a388591a3f09151e8c56c7405
Werner Robitza <werner.robitza@gmail.com>
2021-05-04 15:50:03 +0200

    add further profiling for long sequences

--- test/profile.sh
+++ test/profile.sh
@@ -4,17 +4,21 @@
 
 cd "$(dirname "$0")"
 
-rm -f output.cprof output.pdf && \
-PYTHONPATH=../ \
-python3 \
-    -m cProfile \
-        -s cumtime \
-        -o output.cprof \
-    ../itu_p1203/__main__.py \
-        --cpu-count 1 \
-        --print-intermediate \
-        ../examples/request.json \
-    > /dev/null 2>&1
+rm -f output*.cprof output*.pdf && \
 
-gprof2dot -f pstats -s output.cprof -n 1 | \
-    dot -Tpdf -o output.pdf
+for f in mode0 mode0_long; do
+    echo "Profiling $f"
+    PYTHONPATH=../ \
+    python3 \
+        -m cProfile \
+            -s cumtime \
+            -o output_"$f".cprof \
+        ../itu_p1203/__main__.py \
+            --cpu-count 1 \
+            --print-intermediate \
+            ../examples/"$f".json \
+        > /dev/null 2>&1
+
+    gprof2dot -f pstats -s output_"$f".cprof -n 1 | \
+        dot -Tpdf -o output_"$f".pdf
+done
